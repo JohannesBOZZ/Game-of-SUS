745e4b061f3bbcd62596c46149e8c0e24b5e131b
JohannesBOZZ <maxb12032005@gmail.com>
2022-02-08 14:35:04 +0100

    tjdtj

--- GOL.js
+++ GOL.js
@@ -1,9 +1,23 @@
-const SUS = 210
+const SUS = 69
+let X, Y;
+let r, g, b;
 function setup(){
     createCanvas(420, 420)
 }
 function draw() {
-    background(0, 255, 255);
-    rectMode(CENTER)
-    rect(SUS, SUS, SUS - 100, SUS - 100, SUS,);
+    background(0);
+    fill(r, g, b);
+    noStroke();
+    rect(100, 300, 69 ,69);
+    rect(100 + SUS, 300, 69 ,69);
+    rect(100 + SUS + SUS, 300, 69 ,69);
+    rect(100 + SUS, 300 - SUS, 69 ,69);
+    rect(100 + SUS, 300 -SUS - SUS, 69 ,69);
+    rect(100 + SUS, 300 - SUS - SUS - SUS, 69 ,69);
+    X = random(32, 388);
+    Y = random(32, 388);
+    r = random(255);
+    g = random(255);
+    b = random(255);
+
 }
