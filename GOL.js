ff50b42e43d4c9b11fe15678704033a0a08b65d0
JohannesBOZZ <maxb12032005@gmail.com>
2022-02-11 07:47:54 +0100

    gk

--- GOL.js
+++ GOL.js
@@ -14,10 +14,11 @@ function draw() {
     rect(100 + SUS, 300 - SUS, 69 ,69);
     rect(100 + SUS, 300 -SUS - SUS, 69 ,69);
     rect(100 + SUS, 300 - SUS - SUS - SUS, 69 ,69);
-    X = random(32, 388);
-    Y = random(32, 388);
-    r = random(255);
-    g = random(255);
-    b = random(255);
-
+    if (mouseX > 187){
+        X = random(32, 388);
+        Y = random(32, 388);
+        r = random(255);
+        g = random(255);
+        b = random(255);
+    }
 }
